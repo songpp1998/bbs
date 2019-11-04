4b3480f4db1a8e307c6abd18f43283b5de373f1a
songpp1998 <pp980916@163.com>
2019-11-04 11:30:53 +0800

    项目文档，sql建立

--- src/cn/bbs/test/Test.java
+++ src/cn/bbs/test/Test.java
@@ -1,10 +1,8 @@
 package cn.bbs.test;
 
-import org.eclipse.jdt.internal.compiler.batch.Main;
-
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("这是一个中文测试，是否乱码");//这是一个注释
+		System.out.println();
 	}
 }
