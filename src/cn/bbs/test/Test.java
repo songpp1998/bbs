dccb0967daa976d6da0be2ee4049f1553435b665
songpp1998 <pp980916@163.com>
2019-11-04 10:09:53 +0800

    中文乱码测试

--- src/cn/bbs/test/Test.java
+++ src/cn/bbs/test/Test.java
@@ -5,6 +5,6 @@ import org.eclipse.jdt.internal.compiler.batch.Main;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("111");
+		System.out.println("这是一个中文测试，是否乱码");
 	}
 }
